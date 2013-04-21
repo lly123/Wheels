9ce275dc338d23d60e73ddbd11f71fb576e731db
lly <lly123@gmail.com>
2013-04-21 11:23:40 +0800

    refactoring

--- src/test/java/com/freeroom/di/BeanContextTest.java
+++ src/test/java/com/freeroom/di/BeanContextTest.java
@@ -26,79 +26,79 @@ public class BeanContextTest
     @Test
     public void should_load_bean_given_bean_with_Bean_annotation()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.requiredScope");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.requiredScope");
         assertThat(context.getBeans().size(), is(1));
     }
 
     @Test
     public void should_load_nothing_given_the_package_does_not_exist()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.notExist");
+        final BeanContext context = BeanContext.load("com.freeroom.test.notExist");
         assertThat(context.getBeans().size(), is(0));
     }
 
     @Test
     public void should_load_nothing_given_the_package_exists_but_no_beans()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.nothing");
+        final BeanContext context = BeanContext.load("com.freeroom.test.nothing");
         assertThat(context.getBeans().size(), is(0));
     }
 
     @Test
     public void should_get_bean_given_class_instance()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
         assertThat(context.getBean(Hedgehog.class).isPresent(), is(true));
     }
 
     @Test
     public void should_get_nothing_given_no_bean_with_class()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.dummy");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.dummy");
         assertThat(context.getBean(Dummy.class).isPresent(), is(false));
     }
 
     @Test(expected = NotUniqueException.class)
     public void should_throw_NotUniqueException_given_beans_have_same_parent()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.sameParent");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.sameParent");
         context.getBean(Ladybug.class);
     }
 
     @Test
     public void should_get_bean_given_bean_name()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
         assertThat(context.getBean("Hedgehog").isPresent(), is(true));
     }
 
     @Test
     public void should_not_get_bean_given_no_bean_with_this_name()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.dummy");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.dummy");
         assertThat(context.getBean("Dummy").isPresent(), is(false));
     }
 
     @Test
     public void should_get_bean_given_bean_customized_name()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
         assertThat(context.getBean("Monster").isPresent(), is(true));
     }
 
     @Test(expected = NotUniqueException.class)
     public void should_throw_NotUniqueException_given_beans_have_same_simple_name()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.sameBeanName");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.sameBeanName");
         context.getBean("Trout");
     }
 
     @Test
     public void should_get_bean_by_canonical_name()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.sameBeanName");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.sameBeanName");
 
-        Optional<?> bean = context.getBean("com.freeroom.test.beans.sameBeanName.subPackage.Trout");
+        final Optional<?> bean = context.getBean("com.freeroom.test.beans.sameBeanName.subPackage.Trout");
 
         assertThat(bean.get(), is(instanceOf(Trout.class)));
     }
@@ -112,9 +112,9 @@ public class BeanContextTest
     @Test
     public void should_resolve_bean_by_constructor_injection()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.constructorInjection.subPackage");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.constructorInjection.subPackage");
 
-        Optional<?> caribou = context.getBean("Caribou");
+        final Optional<?> caribou = context.getBean("Caribou");
 
         assertThat(((Caribou)caribou.get()).getMarmot(), is(instanceOf(Marmot.class)));
     }
@@ -122,9 +122,9 @@ public class BeanContextTest
     @Test
     public void should_resolve_bean_by_field_injection()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.fieldInjection");
 
-        Optional<?> hedgehog = context.getBean("Hedgehog");
+        final Optional<?> hedgehog = context.getBean("Hedgehog");
 
         assertThat(((Hedgehog)hedgehog.get()).getSquid(), is(instanceOf(Squid.class)));
     }
@@ -132,9 +132,9 @@ public class BeanContextTest
     @Test
     public void should_resolve_bean_by_setter_injection()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.setterInjection.subPackage");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.setterInjection.subPackage");
 
-        Optional<?> penguin = context.getBean("Penguin");
+        final Optional<?> penguin = context.getBean("Penguin");
 
         assertThat(((Penguin)penguin.get()).getRaccoon(), is(instanceOf(Raccoon.class)));
     }
@@ -142,10 +142,10 @@ public class BeanContextTest
     @Test
     public void should_create_new_beans_given_REQUIRED_scope()
     {
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.requiredScope");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.requiredScope");
 
-        Optional<?> otter1 = context.getBean("Otter");
-        Optional<?> otter2 = context.getBean("Otter");
+        final Optional<?> otter1 = context.getBean("Otter");
+        final Optional<?> otter2 = context.getBean("Otter");
 
         assertThat(otter1.get(), is(not(sameInstance(otter2.get()))));
     }
@@ -153,10 +153,10 @@ public class BeanContextTest
     @Test
     public void should_resolve_bean_from_parent_context()
     {
-        BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne");
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageTwo", parentContext);
+        final BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageTwo", parentContext);
 
-        Optional<?> antelope = context.getBean("Antelope");
+        final Optional<?> antelope = context.getBean("Antelope");
 
         assertThat(((Antelope)antelope.get()).getRhinoceros(), is(instanceOf(Rhinoceros.class)));
     }
@@ -164,25 +164,25 @@ public class BeanContextTest
     @Test
     public void should_get_same_bean_from_parent_context()
     {
-        BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne");
-        BeanContext childContext1 = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageTwo", parentContext);
-        BeanContext childContext2 = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageThree", parentContext);
+        final BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne");
+        final BeanContext childContext1 = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageTwo", parentContext);
+        final BeanContext childContext2 = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageThree", parentContext);
 
-        Optional<?> antelope = childContext1.getBean("Antelope");
-        Optional<?> beetle = childContext2.getBean("Beetle");
+        final Optional<?> antelope = childContext1.getBean("Antelope");
+        final Optional<?> beetle = childContext2.getBean("Beetle");
 
-        Rhinoceros rhinoceros = ((Antelope)antelope.get()).getRhinoceros();
+        final Rhinoceros rhinoceros = ((Antelope)antelope.get()).getRhinoceros();
         assertThat(((Beetle)beetle.get()).getRhinoceros(), is(sameInstance(rhinoceros)));
     }
 
     @Test
     public void should_use_self_contained_bean_when_contexts_overlapped()
     {
-        BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne.subPackage");
-        Optional<?> hamster1 = parentContext.getBean("Hamster");
+        final BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne.subPackage");
+        final Optional<?> hamster1 = parentContext.getBean("Hamster");
 
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne", parentContext);
-        Optional<?> hamster2 = context.getBean("Hamster");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageOne", parentContext);
+        final Optional<?> hamster2 = context.getBean("Hamster");
 
         assertThat(hamster1.get(), is(not(sameInstance(hamster2.get()))));
         assertThat(((Rhinoceros)context.getBean("Rhinoceros").get()).getHamster(), is(sameInstance(hamster2.get())));
@@ -191,8 +191,8 @@ public class BeanContextTest
     @Test
     public void should_get_new_bean_given_bean_is_REQUIRED_scope_in_parent_context()
     {
-        BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageFour.subPackage");
-        BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageFour", parentContext);
+        final BeanContext parentContext = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageFour.subPackage");
+        final BeanContext context = BeanContext.load("com.freeroom.test.beans.parallelPackages.packageFour", parentContext);
 
         final Owl owl1 = ((Falcon)context.getBean("Falcon").get()).getOwl();
         final Owl owl2 = ((Falcon)context.getBean("Falcon").get()).getOwl();
