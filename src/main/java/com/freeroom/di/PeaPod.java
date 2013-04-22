c1e79d20a8db201eb00b8a6784a97725ae46bc56
lly <lly123@gmail.com>
2013-04-22 12:56:08 +0800

    should_be_ready_after_construct_bean

--- src/main/java/com/freeroom/di/PeaPod.java
+++ src/main/java/com/freeroom/di/PeaPod.java
@@ -2,15 +2,18 @@ package com.freeroom.di;
 
 import com.freeroom.di.annotations.Scope;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 class PeaPod extends Pod
 {
+    private final Class<?> factoryClass;
     private final Method beanConstructor;
 
-    public PeaPod(final Method beanConstructor)
+    public PeaPod(Class<?> factoryClass, final Method beanConstructor)
     {
         super(beanConstructor.getReturnType());
+        this.factoryClass = factoryClass;
         this.beanConstructor = beanConstructor;
     }
 
@@ -24,5 +27,23 @@ class PeaPod extends Pod
     public String getBeanName()
     {
         return getBeanName(beanConstructor);
+    }
+
+    public void constructBean()
+    {
+        final Object factory = createFactory(factoryClass);
+        try {
+            setBean(beanConstructor.invoke(factory));
+        } catch (Exception ignored) {}
+    }
+
+    private Object createFactory(final Class<?> factoryClass)
+    {
+        try {
+            final Constructor<?> defaultConstructor = factoryClass.getConstructor();
+            return defaultConstructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Factory " + factoryClass + " must have one default constructor.");
+        }
     }
 }
--- src/test/java/com/freeroom/di/PeaPodTest.java
+++ src/test/java/com/freeroom/di/PeaPodTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.lang.reflect.Method;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,7 +16,7 @@ public class PeaPodTest
     @Test
     public void should_get_bean_class()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("dove"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("dove"));
 
         assertThat(peaPod.getBeanClass().equals(Dove.class), is(true));
     }
@@ -23,7 +24,7 @@ public class PeaPodTest
     @Test
     public void should_get_default_bean_name()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("dove"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("dove"));
 
         assertThat(peaPod.getBeanName(), is("com.freeroom.test.beans.beanFactory.Dove"));
     }
@@ -31,7 +32,7 @@ public class PeaPodTest
     @Test
     public void should_get_customized_bean_name()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("toad"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("toad"));
 
         assertThat(peaPod.getBeanName(), is("toad"));
     }
@@ -39,7 +40,7 @@ public class PeaPodTest
     @Test
     public void should_get_bean_scope()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("pheasant"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("pheasant"));
 
         assertThat(peaPod.getScope(), is(Scope.Required));
     }
@@ -47,7 +48,7 @@ public class PeaPodTest
     @Test
     public void should_use_canonical_class_name_as_default_name()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("pheasant"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("pheasant"));
 
         assertThat(peaPod.hasName("com.freeroom.test.beans.beanFactory.Pheasant"), is(true));
     }
@@ -55,7 +56,7 @@ public class PeaPodTest
     @Test
     public void should_check_by_simple_name()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("pheasant"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("pheasant"));
 
         assertThat(peaPod.hasName("Pheasant"), is(true));
     }
@@ -63,9 +64,20 @@ public class PeaPodTest
     @Test
     public void should_be_unready_given_default_status()
     {
-        final PeaPod peaPod = new PeaPod(getBeanConstructor("dove"));
+        final PeaPod peaPod = new PeaPod(null, getBeanConstructor("dove"));
 
         assertThat(peaPod.isBeanReady(), is(false));
+    }
+
+    @Test
+    public void should_be_ready_after_construct_bean()
+    {
+        final PeaPod peaPod = new PeaPod(Hippopotamus.class, getBeanConstructor("dove"));
+
+        peaPod.constructBean();
+
+        assertThat(peaPod.isBeanReady(), is(true));
+        assertThat(peaPod.getBean().get(), is(instanceOf(Dove.class)));
     }
 
     private Method getBeanConstructor(String beanConstructorName)
