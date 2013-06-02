e3251add7627f3d506ff14410fe5fc622d3ae0f5
lly <lly123@gmail.com>
2013-06-02 10:13:16 +0800

    refactoring

---
+++ src/main/java/com/freeroom/di/util/EnumUtil.java
@@ -0,0 +1,24 @@
+package com.freeroom.di.util;
+
+import java.lang.reflect.Type;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static java.lang.Enum.valueOf;
+
+public class EnumUtil
+{
+    public static boolean isEnumType(Type type)
+    {
+        return type instanceof Class && Enum.class.isAssignableFrom((Class)type);
+    }
+
+    public static String enumToString(Enum<?> enumVal)
+    {
+        return enumVal == null ? "" : enumVal.name();
+    }
+
+    public static Enum<?> stringToEnum(Class type, String value)
+    {
+        return isNullOrEmpty(value) ? null : valueOf(type, value);
+    }
+}
